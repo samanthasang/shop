c11a709793b01883ebacb4f7e6761c16029f5cfb
pooriadaloochi <https://gitlab.com/pooriadaloochi1>
2022-02-03 16:04:17 +0330

    add antd to project

---
+++ craco.config.js
@@ -0,0 +1,36 @@
+/* craco.config.js */
+module.exports = {
+  // ...
+};
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { "@primary-color": "#1DA57A" },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+    ],
+  },
+};
