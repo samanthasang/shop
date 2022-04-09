3bc0dccc8b11bf07c9eb8d875ad1bb384440e942
daloochi <pooriadaloochi@gmail.com>
2022-04-09 19:47:33 +0430

    complete history page

---
+++ src/assets/Icons/LocationIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LocationIcon = () => {
+  return (
+    <svg width="14" height="14" viewBox="0 0 45 45">
+      <path d="M24 4c-7.73 0-14 6.27-14 14 0 10.5 14 26 14 26s14-15.5 14-26c0-7.73-6.27-14-14-14zm0 19c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z" />
+      <path d="M0 0h48v48h-48z" fill="none" />
+    </svg>
+  );
+};
+export default LocationIcon;
